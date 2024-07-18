d2f27b19aa16301cceb4b07e08a60996fdb78e96
jakubBone <jakub.bone1990@gmail.com>
2024-07-18 17:51:24 +0200

    Commit 1: Created DatabaseConnection class with basic implementation

---
+++ src/main/java/ConnectionPool.java
@@ -0,0 +1,2 @@
+public class ConnectionPool {
+}
---
+++ src/main/java/DatabaseConnection.java
@@ -0,0 +1,57 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class DatabaseConnection {
+    private final String USER = "jakub_bone";
+    private final String PASSWORD = "password123";
+    private final String DATABASE = "connection_db";
+    private final int PORT_NUMBER = 5432;
+    private final String URL = String.format("jdbc:postgresql://localhost:%d/%s", PORT_NUMBER, DATABASE);
+    private static DatabaseConnection instance;
+    private static Connection connection;
+
+    public DatabaseConnection() {
+        startConnection();
+    }
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public void startConnection() {
+        try {
+            log.info("Attempting to connect with data base");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if(connection != null){
+                log.info("Connection with {} database established on port {}", USER, PORT_NUMBER);
+            } else {
+                log.info("Failed to connect with {} database established on port {}", USER, PORT_NUMBER);
+            }
+        } catch (SQLException ex) {
+            log.error("Error during database connection: {}", ex.getMessage());
+
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public void disconnect() {
+        try {
+            if (connection != null) {
+                connection.close();
+                log.info("Database disconnected");
+            }
+        } catch(SQLException ex){
+            log.error("Error during database disconnection: {}", ex.getMessage());
+        }
+    }
+}
