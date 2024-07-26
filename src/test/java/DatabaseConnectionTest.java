f6c7bc3a95c87bc4440488075a05ff04409aab62
jakubBone <jakub.bone1990@gmail.com>
2024-07-26 16:29:03 +0200

    Commit 17: Added DatabaseConnetionTest

---
+++ src/test/java/DatabaseConnectionTest.java
@@ -0,0 +1,37 @@
+import database.DatabaseConnection;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class DatabaseConnectionTest {
+    static DatabaseConnection databaseConnection;
+    Connection connection;
+    @BeforeEach
+    void setUp(){
+       databaseConnection = new DatabaseConnection();
+    }
+
+    @AfterAll
+    static void closeDown() {
+        databaseConnection.disconnect();
+    }
+
+    @Test
+    @DisplayName("Should test data base connection return")
+    void testGetConnection() throws SQLException {
+        connection = databaseConnection.getConnection();
+
+        assertNotNull(connection);
+    }
+    @Test
+    @DisplayName("Should test if is data base connection opened")
+    void testIsConnectionOpened() throws SQLException {
+        connection = databaseConnection.getConnection();
+
+        assertFalse(connection.isClosed());
+    }
+}
